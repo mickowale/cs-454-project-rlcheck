4a03edfa3ce6a345edd9d1ca664a3a6493fd902d
Michael Tegegn <michaelwalelegn11@gmail.com>
2020-12-02 17:04:18 +0900

    Use actual state and clean up
    - The generator does not work as expected
    - Generates same values per tree
    - TODO: Need to clearly identify when to use target net and policy net

--- bst_example/test.py
+++ bst_example/test.py
@@ -15,85 +15,52 @@ import torch.nn.functional as F
 import torchvision.transforms as T
 import math
 
-# MAX_DEPTH = 4
-
-# def generate_tree(oracle, depth=0):
-#     value = oracle.Select(range(0, 11), 1)
-#     tree = BinarySearchTree(value) 
-#     if depth < MAX_DEPTH and \
-#             oracle.Select([True, False], 2):
-#         tree.left = generate_tree(oracle, depth+1)
-#     if depth < MAX_DEPTH and \
-#             oracle.Select([True, False], 3):
-#         tree.right = generate_tree(oracle, depth+1) 
-#     return tree 
-
-# def is_BST(tree):
-#     return tree.is_bst()
-
-# def fuzz(oracle, validity_fn):
-#     valids = 0
-#  #   print("Starting!", file=sys.stderr)
-#     valid_set = set()
-#     trials = 100000
-#     for i in range(trials):
-#        # print("{} trials, {} valids, {} unique valids             ".format(i+1, valids, len(valid_set)), end ='\r', file=sys.stderr)
-#         tree = generate_tree(oracle)
-#         is_valid = validity_fn(tree)
-#         if is_valid:
-#             valids += 1
-#             if tree.__repr__() not in valid_set:
-#                 valid_set.add(tree.__repr__())
-#                 oracle.reward(20)
-#             else:
-#                 oracle.reward(0)
-#         else:
-#             oracle.reward(-1)
-#     sizes = [valid_tree.count("(") for valid_tree in valid_set]
-#     print("{} trials, {} valids, {} unique valids".format(trials, valids, len(valid_set)), end ='\n')
-#   #  print("\ndone!", file=sys.stderr)
-#     print(Counter(sizes))
-#     # print("soemthing")
-# if __name__ == '__main__':
-#     print("====Random====")
-#     mo = MockOracle()
-#     fuzz(mo, is_BST)
-#     print("====Sequence====")
-#     oracle_s = Oracle(sequence_ngram_fn(4), epsilon=0.25)
-#     fuzz(oracle_s, is_BST)
-#     print("====Tree====")
-#     oracle_t = Oracle(parent_state_ngram_fn(4, MAX_DEPTH), epsilon=0.25)
-#     fuzz(oracle_t, is_BST)
-#     print("====Tree L/R====")
-#     oracle_lrt = Oracle(left_right_parent_state_ngram_fn(4, MAX_DEPTH), epsilon=0.25)
-#     fuzz(oracle_lrt, is_BST)
-
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-num_epochs = 2000
 epsilon = 0.25
 gamma = 1
-BATCH_SIZE = 64
+BATCH_SIZE = 16
+MAX_DEPTH = 4
+TRIALS = 100000
+TARGET_UPDATE = 10
 
 
-def reward(coor, done):
-    """
-    Task 6 (optional) - design your own reward function
-    """
+valids = 0
+valid_set = set()
 
-    x, y = coor
-    if coor == goal:
-        return 100
-    elif done:
-        return -100
-    elif coor in [(6,5), (5,6)]:
-        return
-   
-    return 0
-    # return -5 
+def get_reward(tree):
+    global valids
+    global valid_set
+    if tree.is_bst(): # It is a valid tree
+        valids += 1
+        if tree.__repr__() not in valid_set:
+            valid_set.add(tree.__repr__())
+            return 20
+        else:
+            return 10
+    else:
+        return -1
 
+class State(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = [0] * capacity
+        self.record = []
+
+    def push(self, action_taken):
+        prev_state = self.memory.copy()
+        self.memory.append(action_taken)
+        if (len(self) > self.capacity):
+            self.memory = self.memory[1:]
+        self.record.append((prev_state, action_taken, self.memory.copy()))
+
+    def collect_history():
+        return self.record
+
+    def __len__(self):
+        return len(self.memory)
 
 class ReplayMemory(object):
     def __init__(self, capacity):
@@ -102,20 +69,12 @@ class ReplayMemory(object):
         self.position = 0
 
     def push(self, transition):
-
-        """
-        Task 3 - 
-        push input: "transition" into replay meory
-        """
         self.memory.insert(0, transition)
         if (len(self) > self.capacity):
             self.memory = self.memory[:self.capacity]
 
+    # Used to sample some transitions to optimized upon
     def sample(self, batch_size):
-        """
-        Task 3 - 
-        give a batch size, pull out batch_sized samples from the memory
-        """
         if batch_size > len(self):
             return self.memory.copy()
         else:
@@ -127,22 +86,15 @@ class ReplayMemory(object):
 class DQN(nn.Module):
     def __init__(self):
         super(DQN, self).__init__()
-        """
-        Task 1 -
-        generate your own deep neural network
-        """
+
         self.linear1 = nn.Linear(6, 16)
         self.linear2 = nn.Linear(16, 32)
         self.linear3 = nn.Linear(32, 10)  
         self.relu = nn.ReLU()          
 
 
-
     def forward(self, x):
-        """
-        Task 1 - 
-        generate your own deep neural network
-        """
+
         x = self.linear1(x)
         x = self.relu(x)
         x = self.linear2(x)
@@ -154,26 +106,20 @@ def state_to_tensor(state):
     a = np.array(state, dtype=np.float32)
     return  torch.from_numpy(a)
 
-# transition = (cur_state, idx_action, reward_val, new_state)
 def optimize_model():
     if len(memory) < BATCH_SIZE:
         return
     transitions = memory.sample(BATCH_SIZE)
-    """
-    Task 4: optimize model
-    """
 
     y_values = []
     p_values = []
+
+    # Collect the y and p values to calculate the loss
     for trans in transitions:   
         (state, idx_action, reward, next_state) = trans
-        # if done:
-        #     y = torch.from_numpy(np.array(reward, dtype=np.float32))
-        # else:
-        #     y = reward + gamma * torch.max(target_net(next_state)) 
         y = torch.from_numpy(np.array(reward, dtype=np.float32))
 
-
+        state = state_to_tensor(state)
         p = policy_net(state)[idx_action]
         y_values.append(y.unsqueeze(0))
         p_values.append(p)
@@ -191,64 +137,42 @@ target_net.load_state_dict(policy_net.state_dict())
 target_net.eval()
 
 optimizer = optim.RMSprop(policy_net.parameters())
-memory = ReplayMemory(1000)
+memory = ReplayMemory(100)
 
 
-# transition = (cur_state, idx_action, reward_val, new_state)
 def select_action(state):
-    """
-    Task 2: select action
-    """
     if random.random() < epsilon:
-        # Fixme: Why does index action become 10 sometimes with this code
-        # a = random.choices([1,2,3,4,5,6,7,8,9,10])
         a = random.choices([0,1,2,3,4,5,6,7,8,9])
-        # print(a)
         return torch.from_numpy(np.array(a))
 
     else:   
-
-        # FixMe: Use an abstracted form of the state
-        state = [8,2,0,-1,2,3]
-        a = np.array(state, dtype=np.float32)   
-        t = torch.from_numpy(a)
-        output = policy_net(t)
+        state = state_to_tensor(state.memory)
+        output = policy_net(state)
         return torch.argmax(output.detach()).unsqueeze(0)
 
 
+
+# TODO: When do we use the target network?
 def select_exploit_action(model, state):
-    # a = state_astraction(state)
-    state = [8,2,0,-1,2,3]
-    a = np.array(state, dtype=np.float32)
-    t = torch.from_numpy(a)
-    output = model(t)
+    state = state_to_tensor(state.memory)
+    output = model(state)
     return torch.argmax(output)
 
 
-state = ReplayMemory(10)
 memory = ReplayMemory(100)
-state_action_record = []
 
-MAX_DEPTH = 10
-def generate_tree(depth=0):
-    global state
-    global state_action_record
-    if (depth == 0): # Clear the state when generating a new tree
-        state = ReplayMemory(10)
-        state_action_record = []
-    value = select_exploit_action(policy_net, state)
+def generate_tree(state, depth=0):
+    value = select_exploit_action(target_net, state)
     state.push(value)
-    state.memory = [8,2,0,-1,2,3]
-    state_action_record.append((state.memory.copy(), value))
     tree = BinarySearchTree(value) 
     if depth < MAX_DEPTH and \
             random.choice([True, False]):
-        state.push(0) # To denote left subtree existence
-        tree.left = generate_tree(depth+1)
+        # state.push(0) # To denote left subtree existence
+        tree.left = generate_tree(state, depth+1)
     if depth < MAX_DEPTH and \
             random.choice([True, False]):
-        state.push(-1) # To denote right subtree existence
-        tree.right = generate_tree(depth+1) 
+        # state.push(-1) # To denote right subtree existence
+        tree.right = generate_tree(state, depth+1) 
     return tree 
 
 
@@ -256,72 +180,26 @@ def fuzz():
 
     valids = 0
     valid_set = set()
-    trials = 100000
-    TARGET_UPDATE = 5
-    for i in range(trials):
-        # print(i)
-        # while not done:
-        tree = generate_tree()
+    for i in range(TRIALS):
+        state = State(6)
+        tree = generate_tree(state)
         print(tree)
-        # reward = get_reward(tree)
-        reward = 10
-
-        for state_action in state_action_record:
-            # cur_state = [x, y, o_i] + sensor 
-            (state, action) = state_action
-            a = np.array(state, dtype=np.float32)
-            cur_state = torch.from_numpy(a)
-
-
-            action_t = select_action(cur_state).long()
-            # idx_action = action_t.item()
-            # action = actions[idx_action]
-            # (new_x, new_y), new_ori, new_sensor, done = action()
-            # new_o_i = orientation.index(new_ori)
-
-            # reward_val = reward((new_x,new_y),done)
-            reward_val = reward
-
-
-            new_state = state.copy()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            a = np.array(new_state, dtype=np.float32)
-            new_state = torch.from_numpy(a) 
-            reward_val = torch.from_numpy(np.array(reward_val))
-            # transition = (cur_state, action_t, reward_val, new_state)
-
-            transition = (cur_state, action_t, reward_val, new_state)
+        reward = get_reward(tree)
+        for (cur_state, action, new_state) in state.record:
+            new_state = state_to_tensor(new_state)
+            reward_val = torch.from_numpy(np.array(reward))
+            transition = (cur_state, action, reward_val, new_state)
 
             memory.push(transition)
-            # (x, y), ori, sensor = (new_x, new_y), new_ori, new_sensor
             optimize_model()
 
         if (i%TARGET_UPDATE == 0):
             target_net.load_state_dict(policy_net.state_dict())
-        # if (i%(num_epochs//10) == 0):
-        #     epsilon *= 0.85
         print("epoch = ", i)
 
 
 
 
 fuzz()
-"""
-Task 5 - save your policy net
-"""
-torch.save(policy_net.state_dict(), "my_model")
+# torch.save(policy_net.state_dict(), "my_model")
 
