09f7727cea83c08d61272f4f95fbb38e474b64ab
webi dabuse <web64td@gmail.com>
2020-12-17 17:43:58 +0900

    Rnn trial and result measure

--- bst_example/bst_fuzz.py
+++ bst_example/bst_fuzz.py
@@ -4,12 +4,13 @@ from bst import BinarySearchTree
 from collections import Counter
 from oracles import MockOracle, Oracle
 from state_abstraction import parent_state_ngram_fn, left_right_parent_state_ngram_fn, sequence_ngram_fn
-
-
+import csv
+import time
+import os,psutil
 MAX_DEPTH = 4
 
 def generate_tree(oracle, depth=0):
-    value = oracle.Select(range(0, 11), 1)
+    value = oracle.Select(range(0, 10), 1)
     tree = BinarySearchTree(value) 
     if depth < MAX_DEPTH and \
             oracle.Select([True, False], 2):
@@ -22,39 +23,59 @@ def generate_tree(oracle, depth=0):
 def is_BST(tree):
     return tree.is_bst()
 
-def fuzz(oracle, validity_fn):
-    valids = 0
- #   print("Starting!", file=sys.stderr)
-    valid_set = set()
-    trials = 100000
-    for i in range(trials):
-#        print("{} trials, {} valids, {} unique valids             ".format(i+1, valids, len(valid_set)), end ='\r', file=sys.stderr)
-        tree = generate_tree(oracle)
-        is_valid = validity_fn(tree)
-        if is_valid:
-            valids += 1
-            if tree.__repr__() not in valid_set:
-                valid_set.add(tree.__repr__())
-                oracle.reward(20)
+def fuzz(oracle, validity_fn,filename):
+    filename += "_range_10.csv"
+    with open(filename,'w+') as file:
+        valids = 0
+     #   print("Starting!", file=sys.stderr)
+        valid_set = set()
+        trials = 100000
+        for i in range(trials):
+    #        print("{} trials, {} valids, {} unique valids             ".format(i+1, valids, len(valid_set)), end ='\r', file=sys.stderr)
+            tree = generate_tree(oracle)
+            is_valid = validity_fn(tree)
+            if is_valid:
+                valids += 1
+                if tree.__repr__() not in valid_set:
+                    valid_set.add(tree.__repr__())
+                    oracle.reward(20)
+                else:
+                    oracle.reward(0)
             else:
-                oracle.reward(0)
-        else:
-            oracle.reward(-1)
-    sizes = [valid_tree.count("(") for valid_tree in valid_set]
-    print("{} trials, {} valids, {} unique valids".format(trials, valids, len(valid_set)), end ='\r')
-  #  print("\ndone!", file=sys.stderr)
-    print(Counter(sizes))
+                oracle.reward(-1)
+            file.write(str(i) + "," + str(valids) + "," + str(len(valid_set)))
+            file.write("\n")
+        sizes = [valid_tree.count("(") for valid_tree in valid_set]
+        print("{} trials, {} valids, {} unique valids".format(trials, valids, len(valid_set)), end ='\n')
+
+      #  print("\ndone!", file=sys.stderr)
+        file.close()
+        print(Counter(sizes))
 
 if __name__ == '__main__':
-    print("====Random====")
-    mo = MockOracle()
-    fuzz(mo, is_BST)
-    print("====Sequence====")
-    oracle_s = Oracle(sequence_ngram_fn(4), epsilon=0.25)
-    fuzz(oracle_s, is_BST)
+    pid = os.getpid( )
+    ps = psutil.Process(pid)
+    # print("====Random====")
+    # mo = MockOracle()
+    # fuzz(mo, is_BST,"random")
+    # memoryuse = ps.memory_info()
+    # print(memoryuse)
+    # print(memoryuse.rss/1024**2)
+    # print("====Sequence====")
+    # oracle_s = Oracle(sequence_ngram_fn(4), epsilon=0.25)
+    # fuzz(oracle_s, is_BST,"sequence")
+    # memoryuse = ps.memory_info()
+    # print(memoryuse)
+    # print(memoryuse.rss/1024**2)
     print("====Tree====")
     oracle_t = Oracle(parent_state_ngram_fn(4, MAX_DEPTH), epsilon=0.25)
-    fuzz(oracle_t, is_BST)
-    print("====Tree L/R====")
-    oracle_lrt = Oracle(left_right_parent_state_ngram_fn(4, MAX_DEPTH), epsilon=0.25)
-    fuzz(oracle_lrt, is_BST)
+    fuzz(oracle_t, is_BST,"Tree")
+    memoryuse = ps.memory_info()
+    print(memoryuse)
+    print(memoryuse.rss/1024**2)
+    # print("====Tree L/R====")
+    # oracle_lrt = Oracle(left_right_parent_state_ngram_fn(4, MAX_DEPTH), epsilon=0.25)
+    # fuzz(oracle_lrt, is_BST,"Tree_LR")
+    # memoryuse = ps.memory_info()
+    # print(memoryuse)
+    # print(memoryuse.rss/1024**2)
---
+++ bst_example/rnn_trial.py
@@ -0,0 +1,257 @@
+import random
+import sys
+from bst import BinarySearchTree
+from collections import Counter
+from oracles import MockOracle, Oracle
+from state_abstraction import parent_state_ngram_fn, left_right_parent_state_ngram_fn, sequence_ngram_fn
+
+import random
+import numpy as np
+import time
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.transforms as T
+import math
+
+
+device = torch.device("cpu" if torch.cuda.is_available() else "cpu")
+
+
+epsilon = 0.25
+gamma = 1
+BATCH_SIZE = 16
+MAX_DEPTH = 4
+TRIALS = 1000
+TARGET_UPDATE = 10
+
+
+valids = 0
+valid_set = set()
+
+def get_reward(tree):
+    global valids
+    global valid_set
+    if tree.is_bst(): # It is a valid tree
+        valids += 1
+        if tree.__repr__() not in valid_set:
+            valid_set.add(tree.__repr__())
+            return 20
+        else:
+            return 10
+    else:
+        return -1
+
+class State(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = [0]*capacity
+        self.record = []
+
+    def push(self, action_taken):
+        prev_state = self.memory.copy()
+        self.memory.append(action_taken)
+        if (len(self) > self.capacity):
+            self.memory = self.memory[1:]
+        self.record.append((prev_state, action_taken, self.memory.copy()))
+
+    def collect_history():
+        return self.record
+
+    def __len__(self):
+        return len(self.memory)
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, transition):
+        self.memory.insert(0, transition)
+        if (len(self) > self.capacity):
+            self.memory = self.memory[:self.capacity]
+
+    # Used to sample some transitions to optimized upon
+    def sample(self, batch_size):
+        if batch_size > len(self):
+            return self.memory.copy()
+        else:
+            return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+class DQN(nn.Module):
+    def __init__(self):
+        super(DQN, self).__init__()
+
+        self.linear1 = nn.Linear(6, 16)
+        self.linear2 = nn.Linear(16, 32)
+        self.linear3 = nn.Linear(32, 10)  
+        self.relu = nn.ReLU()          
+
+
+    def forward(self, x):
+
+        x = self.linear1(x)
+        # x = self.relu(x)
+        x = self.linear2(x)
+        # x = self.relu(x)
+        x = self.linear3(x)
+        return x
+
+class RNN_LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(RNN_LSTM, self).__init__()
+        # print("this is RNN_LSTM")
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size * sequence_length, num_classes)
+
+    def forward(self, x):
+        # Set initial hidden and cell states
+        # print("________ forward")
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+
+        # Forward propagate LSTM
+        x = x.unsqueeze(0)
+        # print("inside forward", x)
+        out, _ = self.lstm(
+            x, (h0, c0)
+        )  # out: tensor of shape (batch_size, seq_length, hidden_size)
+        out = out.reshape(out.shape[0], -1)
+
+        # Decode the hidden state of the last time step
+        out = self.fc(out)
+        return out
+def state_to_tensor(state):
+    a = np.array(state, dtype=np.float32)
+    return  torch.from_numpy(a)
+
+def optimize_model():
+    if len(memory) < BATCH_SIZE:
+        return
+    transitions = memory.sample(BATCH_SIZE)
+
+    y_values = []
+    p_values = []
+
+    # Collect the y and p values to calculate the loss
+    for trans in transitions:   
+        (state, idx_action, reward, next_state) = trans
+        y = torch.from_numpy(np.array(reward, dtype=np.float32))
+        state = state_to_tensor(state)
+        state = state.unsqueeze(0)
+        p = policy_net(state)[0,idx_action]
+        y_values.append(y.unsqueeze(0))
+        p_values.append(p)
+    y_values = torch.stack(y_values).detach()
+    p_values = torch.stack(p_values)
+    loss = F.mse_loss(p_values, y_values)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+input_size = 6
+hidden_size = 100
+num_layers = 2
+num_classes = 10
+sequence_length = 1
+
+
+policy_net = RNN_LSTM(input_size, hidden_size, num_layers, num_classes).to(device)
+target_net = RNN_LSTM(input_size, hidden_size, num_layers, num_classes).to(device)
+target_net.load_state_dict(policy_net.state_dict())
+target_net.eval()
+
+optimizer = optim.RMSprop(policy_net.parameters())
+memory = ReplayMemory(100)
+
+
+def select_action(state):
+    if random.random() < epsilon:
+        a = random.choices([0,1,2,3,4,5,6,7,8,9])
+        return torch.from_numpy(np.array(a))
+
+    else:   
+        state = state_to_tensor(state.memory)
+        # print(state.size())
+        state = state.unsqueeze(0)
+        # print(state.size())
+        output = policy_net(state)
+        return torch.argmax(output)
+
+
+
+# TODO: When do we use the target network?
+def select_exploit_action(model, state):
+    state = state_to_tensor(state.memory)
+    # print(state.size())
+    state = state.unsqueeze(0)
+    # print(state.size())
+    output = model(state)
+    return torch.argmax(output)
+
+
+memory = ReplayMemory(100)
+
+def generate_tree(state, depth=0):
+    value = select_exploit_action(policy_net,state)
+    state.push(value)
+    tree = BinarySearchTree(value) 
+    if depth < MAX_DEPTH and \
+            random.choice([True, False]):
+        # state.push(0) # To denote left subtree existence
+        tree.left = generate_tree(state, depth+1)
+    if depth < MAX_DEPTH and \
+            random.choice([True, False]):
+        # state.push(-1) # To denote right subtree existence
+        tree.right = generate_tree(state, depth+1) 
+    return tree 
+
+
+def fuzz():
+    global valid_set, valids
+    
+    valid_set = set()
+    for i in range(TRIALS):
+        print("epoch = ", i)
+        state = State(6)
+        tree = generate_tree(state)
+        print(tree)
+        reward = get_reward(tree)
+        for (cur_state, action, new_state) in state.record:
+            new_state = state_to_tensor(new_state)
+            reward_val = torch.from_numpy(np.array(reward))
+            transition = (cur_state, action, reward_val, new_state)
+            memory.push(transition)
+            optimize_model()
+
+        if (i%TARGET_UPDATE == 0):
+            target_net.load_state_dict(policy_net.state_dict())
+        print("Number of valids       : ", valids)
+        print("Number of unique valids: ", len(valid_set))
+        
+
+
+
+
+fuzz()
+# torch.save(policy_net.state_dict(), "my_model")
+
+
+
+
+
+
+
+
+
+
+
+
+
